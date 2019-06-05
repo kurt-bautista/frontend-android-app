846141e593b72a98c8f0c6f0c3b18664a9916e8e
Kurt Vincent Bautista <kurt.v@outlook.com>
2019-06-05 12:13:01 +0800

    Use DataBindingUtil to set content view

--- app/src/main/java/com/example/frontendandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/frontendandroidapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.frontendandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.frontendandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
