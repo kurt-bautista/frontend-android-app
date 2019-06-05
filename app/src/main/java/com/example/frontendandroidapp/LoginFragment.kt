3c9f4fdb8cb02d87e312fb46a4b50e2c92aaf994
Kurt Vincent Bautista <kurt.v@outlook.com>
2019-06-05 12:14:25 +0800

    Create login fragment

---
+++ app/src/main/java/com/example/frontendandroidapp/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.example.frontendandroidapp
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.frontendandroidapp.databinding.FragmentLoginBinding
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+
+}
