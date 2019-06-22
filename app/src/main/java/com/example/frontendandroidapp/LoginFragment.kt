bf2a24e677d2f76422848fbc08b124ebff26e374
Kurt Vincent Bautista <kurt.v@outlook.com>
2019-06-23 00:16:17 +0800

    Set up navigation on login fragment

--- app/src/main/java/com/example/frontendandroidapp/LoginFragment.kt
+++ app/src/main/java/com/example/frontendandroidapp/LoginFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.frontendandroidapp.databinding.FragmentLoginBinding
 
 /**
@@ -21,6 +23,9 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.tvSignUp.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_signupFragment)
+        }
         return binding.root
     }
 
